c8296c3bafa5bbbd7426e0801d7f72c7416519b6
Rony Rajput <67194882+rrohit-12@users.noreply.github.com>
2020-08-18 02:00:27 +0530

    Add files via upload
    input and output

---
+++ input and output basic number.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	int q,r,t,y,u;
+	cin >> N;	
+	cin>>q>>r>>t>>y>>u;
+	cout << q<<" "<<r<<" "<<t<<" "<<y<<" "<<u<<" ";
+	cout<<endl;
+}
+
+
